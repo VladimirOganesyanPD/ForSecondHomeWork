619a5c2393bf987067dba0f238471aa827ac8695
Vladimir Oganesyan <mr.lalochkina@mail.ru>
2024-03-06 00:31:50 +0300

    Add gitignore func. Add str_func for upper scale. Add str_func for upper scalex3.(2) with docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,27 @@
 def to_uppercase(input_string):
+    """
+    Преобразует строку в верхний регистр.
+
+    input_string это Входная строка, которую нужно преобразовать в верхний регистр.
+
+    Возвращает:
+    str это Строка в верхнем регистре.
+    """
     return input_string.upper()
+def to_uppercase(input_string):
+    """
+    Преобразует строку в верхний регистр.
+
+    param input_string это Входная строка.
+    return это Строка в верхнем регистре.
+    """
+    return input_string.upper()
+
+def capitalize_words(input_string):
+    """
+    Делает заглавными первые буквы каждого слова в строке.
+
+    param input_string это Входная строка.
+    return это Строка, в которой первые буквы каждого слова заглавные.
+    """
+    return ' '.join(word.capitalize() for word in input_string.split())
